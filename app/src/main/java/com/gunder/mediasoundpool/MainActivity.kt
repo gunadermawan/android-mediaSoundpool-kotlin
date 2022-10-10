0119c123ed7b251ffbb09cc60c39142cc432c269
itselaras <gunadermawan62@gmail.com>
2022-10-10 18:48:13 +0700

    create function to play sound

--- app/src/main/java/com/gunder/mediasoundpool/MainActivity.kt
+++ app/src/main/java/com/gunder/mediasoundpool/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.gunder.mediasoundpool
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.SoundPool
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.gunder.mediasoundpool.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var sp: SoundPool
+    private var soundId: Int = 0
+    private var spLoaded = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        sp = SoundPool.Builder()
+            .setMaxStreams(10)
+            .build()
+        sp.setOnLoadCompleteListener { _, _, status ->
+            if (status == 0) {
+                spLoaded = true
+            } else {
+                Toast.makeText(this, "Load failed!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        soundId = sp.load(this, R.raw.clinking_glasses, 1)
+        binding.btnPlay.setOnClickListener {
+            if (spLoaded) {
+                sp.play(soundId, 1f, 1f, 0, 0, 1f)
+            }
+        }
     }
 }
